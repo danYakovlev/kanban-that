b366ed1575d2d521f02f808487ce6ead88fc0718
danYakovlev <dan23yakovlev@gmail.com>
2017-10-02 01:30:15 +0400

    base project structure

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './App';
+
+
+const rootElement = document.getElementById('root');
+
+const renderApp = Component => {
+    render(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        rootElement    
+    );
+}
+
+renderApp(App);
+
+if (module.hot) module.hot.accept('./App', () => renderApp(App));
---
+++ webpack.config.babel.js
@@ -0,0 +1,49 @@
+import path from 'path';
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const isProd = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+    entry: [
+        'react-hot-loader/patch',
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/index.js',
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname + '/dist'),
+        publicPath: '/',
+    },
+    devtool: 'inline-source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loaders: ['react-hot-loader/webpack', 'babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'KanbanThat',
+            template: 'src/index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+    ],
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+        historyApiFallback: true,
+        hot: true,
+    }
+}
