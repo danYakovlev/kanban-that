5350787512096d78fba3b9395838af300d75d78e
danYakovlev <dan23yakovlev@gmail.com>
2017-10-10 01:48:53 +0400

    added: base App styles, fonts link

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import { Provider } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import Routes from './Routes';
 
 import stores from './stores/index';
+
+import './App.scss';
+
 
 class App extends Component {
     render() {
         return (
             <Provider {...stores} >
                 <Router>
-                    <div>
-                        <Routes />
-                        <DevTools />
-                    </div>
+                    <Routes />
                 </Router>
             </Provider>
         );
